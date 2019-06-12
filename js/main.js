923e809febaeacc505ce82eec606421bb4f62f4f
Matt <matt@mspacecreative.com>
2019-06-12 15:10:45 -0300

    c

--- js/main.js
+++ js/main.js
@@ -135,7 +135,7 @@ $(document).ready(function() {
 			$(this).toggleClass('reveal');
 		});
 	} else {
-		$('.our-team-window').on('click touchstart', function () {
+		$('.our-team-window').on('click touch', function () {
 			$(this).toggleClass('reveal');
 		});
 	}
