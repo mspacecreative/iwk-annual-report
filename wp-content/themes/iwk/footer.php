a4950fd57a142bd5a3c2c27e99c0765b4356e720
Matt <matt@mspacecreative.com>
2019-06-20 08:37:38 -0300

    c

--- wp-content/themes/iwk/footer.php
+++ wp-content/themes/iwk/footer.php
@@ -1,48 +1,60 @@
 			<!-- FOOTER -->
+			<?php if( have_rows('footer', 'options') ): ?>
 			<footer class="section fp-auto-height">
 				<div class="inner light top-bottom-padding max-1600">
+					<?php while( have_rows('footer', 'options') ): the_row(); ?>
 					<div class="clearfix flex-container">
+						<?php
+						$image = get_sub_field('footer_logo', 'options');
+						$size = 'full';
+						if ( $image ) { ?>
 						<div class="one_third">
-							<img src="img/iwk_logo_white.png">
+							<?php echo wp_get_attachment_image( $image, $size ); ?>
 						</div>
+						<?php } ?>
 						<div class="one_third">
-							<p>The IWK Health Centre is the Maritime region's leading health care and research centre dedicated to the well-being of women, children, youth and families. In addition to providing highly specialized and complex care, the IWK provides certain primary care services and is a strong advocate for the health of families. The IWK is a global leader in research and knowledge sharing, and a partner in educating the next generation of health professionals.</p>
+							<p><?php the_sub_field('site_description', 'options'); ?></p>
 						</div>
+						<?php if( have_rows('footer_external_link', 'options') ): ?>
 						<div class="one_third last_column">
+							<?php while( have_rows('footer_external_link', 'options') ): the_row(); ?>
 							<ul class="links">
-								<li><a href="http://www.iwk.nshealth.ca/" target="_blank">Learn more about the IWK Health Centre »</a></li>
+								<li><a href="<?php the_sub_field('link', 'options'); ?>" target="_blank"><?php the_sub_field('label', 'options'); ?> &raquo;</a></li>
 							</ul>
+							<?php endwhile; ?>
 						</div>
+						<?php endif; ?>
 					</div>
 					<div class="clearfix" style="margin-top: 20px;">
 						<div class="lcol-full">
 							<div class="footer-menu">
 								<ul>
 									<li class="contact-slide"><a href="javascript:void(0)">Contact Us</a></li>
-							    	<li><a href="http://www.iwk.nshealth.ca/page/terms-use-disclaimer" target="_blank">Terms of Use</a></li>
-							    	<li><a href="http://www.iwk.nshealth.ca/page/privacy-policy" target="_blank">Privacy Policy</a></li>
+									<?php if( have_rows('link_list', 'options') ): ?>
+									<?php while( have_rows('link_list', 'options') ): the_row(); ?>
+							    	<li><a href="<?php the_sub_field('list_link', 'options'); ?>" target="_blank"><?php the_sub_field('list_label', 'options'); ?></a></li>
+									<?php endwhile; ?>
+									<?php endif; ?>
 								</ul>
 							</div>
 						</div>
 						<div class="rcol-full">
-							<p>© Copyright 2019. All Rights Reserved.</p>
+							<p>&copy; 2019 <?php bloginfo( 'name' ); ?>. All Rights Reserved.</p>
 						</div>
 					</div>
+					<?php endwhile; ?>
 				</div>
 			</footer>
+			<?php endif; ?>
 			<!-- / FOOTER -->
 		
 			<!-- CONTACT SLIDE-OUT BOX -->
+			<?php if ( get_field('contact_slideout', 'options') ): ?>
 			<div class="contact-slideout">
 				<div class="contact-toggle"><i class="fa fa-times"></i></div>
-				<p>The IWK Health Centre wants, encourages and welcomes patients and families to give us feedback, and we are currently working on new ways to make this easier.</p>
-				<p>Please be advised that the IWK has a dedicated feedback line and e-mail address available for patients and families to submit feedback at anytime. The toll free number is <a style="color: #741f38;" href="tel:18554952273">1.855.495.2273 (IWK CARE)</a> or the Halifax local number is <a style="color: #741f38;" href="tel:19024707435">902.470.7435</a>. The e-mail address is <a style="color: #741f38;" href="mailto:feedback@iwk.nshealth.ca">feedback@iwk.nshealth.ca</a>.</p>
-				
-				<ul>
-					<li>Communications &amp; Public Affairs: <a style="color: #741f38;" href="tel:19024706740">902.470.6740</a></li>
-					<li style="margin-bottom: 0;">Administrative Switchboard: <a style="color: #741f38;" href="tel:19024708888">902.470.8888</a></li>
-				</ul>
+				<?php the_field('contact_slideout', 'options'); ?>
 			</div>
+			<?php endif; ?>
 			<!-- / CONTACT SLIDE-OUT BOX -->
 			
 		</div>
