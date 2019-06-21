bf05c2f2a7b5116a6ce58fc90a86ddec74e9ed97
Matt <matt@mspacecreative.com>
2019-06-21 14:06:17 -0300

    c

--- wp-content/themes/iwk/404.php
+++ wp-content/themes/iwk/404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="wrap">
-	<div class="inner">
+	<div class="inner" class="top-bottom-padding">
 		<h2><?php esc_html_e('Page Not Found.'); ?></h2>
 		<p><?php esc_html_e('Please use navigation to find the content you&#8217;re looking for.'); ?></p>
 	</div>
