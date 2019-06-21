d17b72b08b9717b95ac455e97ca66ded2222624d
Matt <matt@mspacecreative.com>
2019-06-21 15:09:01 -0300

    c

--- wp-content/themes/iwk/404.php
+++ wp-content/themes/iwk/404.php
@@ -4,7 +4,7 @@
 	<div class="inner align-center" style="padding: 10% 0;">
 		<h2><?php esc_html_e('Page Not Found.'); ?></h2>
 		<p><?php esc_html_e('Please use navigation to find the content you&#8217;re looking for.'); ?></p>
-		<p><?php esc_html_e('Or, click ' ?><a href="<?php echo home_url(); ?>">here</a><?php esc_html_e(' to go to the home page.'); ?></p>
+		<p><?php esc_html_e('Or, click '); ?><a href="<?php echo home_url(); ?>">here</a><?php esc_html_e(' to go to the home page.'); ?></p>
 	</div>
 </div>
 
