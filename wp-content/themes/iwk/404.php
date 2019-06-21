28db3b5fd128454de2e89399463536850d7b7512
Matt <matt@mspacecreative.com>
2019-06-21 14:05:02 -0300

    c

---
+++ wp-content/themes/iwk/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="wrap">
+	<div class="inner">
+		<h2><?php esc_html_e('Page Not Found.'); ?></h2>
+		<p><?php esc_html_e('Please use navigation to find the content you&#8217;re looking for.'); ?></p>
+	</div>
+</div>
+
+<?php get_footer(); ?>
