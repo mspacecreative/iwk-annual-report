89e0da47e3cf0a7fb5adfea6bb76d9b2b43106a5
Matt <matt@mspacecreative.com>
2019-06-22 15:37:19 -0300

    c

--- wp-content/themes/iwk/functions.php
+++ wp-content/themes/iwk/functions.php
@@ -77,7 +77,7 @@ function titleTag() {
 $targetoptions = 'time_input';
 $datetime = get_field($targetoptions, false);
 $datetime = DateTime::createFromFormat('F j', 'Y H:i:s', $datetime);
-$currentdate =  date('F j', 'Y H:i:s');
+$currentdate =  new DateTime('F j', 'Y H:i:s');
 $futuredate = new DateTime($datetime);
 
 if($currentdate > $futuredate) {
