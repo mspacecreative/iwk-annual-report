3185a1f6bb3be40e95a6d0b54c928366eea7d5cc
Matt <matt@mspacecreative.com>
2019-06-22 18:32:24 -0300

    c

--- wp-content/themes/iwk/functions.php
+++ wp-content/themes/iwk/functions.php
@@ -74,9 +74,10 @@ if (function_exists('add_theme_support')) {
 function titleTag() {
  add_theme_support( 'title-tag' );
 }
+
 $retrievefield = 'time_input';
 $currentdate = date('F j', 'Y H:i:s');
-$futuredate = get_field($retrievefield, 2);
+$futuredate = echo get_field($retrievefield, 2);
 
 if($futuredate < $currentdate) {
 	$my_plugin = $plugin_path . 'mspace_timer/mspace_timer.php';
