e37aa4cfeada15f6d0e8143ee27af4052e61a93f
Matt <matt@mspacecreative.com>
2019-06-22 19:02:29 -0300

    c

--- wp-content/themes/iwk/functions.php
+++ wp-content/themes/iwk/functions.php
@@ -77,7 +77,7 @@ function titleTag() {
 
 $retrievefield = 'time_input';
 $retrieveall = get_field($retrievefield, 2);
-$currentdate = new DateTime();
+$currentdate = new DateTime('F j', 'Y H:i:s');
 $futuredate = new DateTime($retrieveall);
 
 if ( $currentdate > $futuredate ) {
