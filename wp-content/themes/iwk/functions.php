719d001b16209e14da22d62ded9d27243db6b900
Matt <matt@mspacecreative.com>
2019-06-23 09:23:37 -0300

    c

--- wp-content/themes/iwk/functions.php
+++ wp-content/themes/iwk/functions.php
@@ -77,7 +77,7 @@ function titleTag() {
 
 $retrievefield = 'disable_plugin';
 $retrieveall = get_field($retrievefield, 2);
-$currentdate = new date('Y-m-d H:i:s');
+$currentdate = date('Y-m-d H:i:s');
 $futuredate = new DateTime($retrieveall);
 
 if ( $currentdate > $futuredate ) {
